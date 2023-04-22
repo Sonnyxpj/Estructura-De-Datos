3a531617d04e1e62c99cfc8bad8eda9fc26333d6
Benjamín Garrido <benjamin.garrido.j@gmail.com>
2023-04-22 19:48:29 -0400

    #

--- Algoritmo de ordenamiento/ordenamiento.c
+++ Algoritmo de ordenamiento/ordenamiento.c
@@ -9,12 +9,17 @@ void lectura(int array[]);
 void burbuja(int array[], int contador);
 void shell(int array[], int contador);
 
+void swap(int* a, int* b);
+int particion (int array[], int bajo, int alto);
+void quickSort(int array[], int bajo, int alto);
+
+
 struct rusage usada;
 
 int main(){
 
     getrusage(RUSAGE_SELF, &usada);
-    struct timeval t_1, t_2;
+    struct timeval t_1, t_2;  
     double tiempo; 
     int array[N], contador;
 
@@ -24,21 +29,24 @@ int main(){
 
     gettimeofday(&t_1,NULL); 
 
-    burbuja(array, contador);
+    //burbuja(array, contador);
     //shell(array, contador);
+    quickSort(array, 0, contador-1);
 
     gettimeofday(&t_2,NULL);
 
-    tiempo = (t_2.tv_sec - t_1.tv_sec)1000 + (t_2.tv_usec - t_1.tv_usec)/1000;
+    tiempo = (t_2.tv_sec - t_1.tv_sec)*1000 + (t_2.tv_usec - t_1.tv_usec)/1000;
 
     printf("%d;%g;%ldB\n", contador, tiempo/1000, usada.ru_maxrss);
     }
+    
+    printf("%ldB\n", usada.ru_maxrss);
 
     return 0;
 }
 
 void lectura(int array[]){
-    FILEentrada = fopen("./aleatorios.txt", "r");
+    FILE *entrada = fopen("datos.txt", "r");
     int i = 0;
 
     for(i = 0; i < N ; i++){
@@ -66,14 +74,14 @@ void burbuja(int array[], int contador){
         }
     }
 }
-
+    
 void shell(int array[], int contador){
     int salto, cambios, i, aux;
 
     for(salto = contador/2; salto!=0; salto = salto/2){
 
         cambios = 1;
-
+        
         while (cambios != 0){
 
             cambios=0; 
@@ -81,7 +89,7 @@ void shell(int array[], int contador){
             for(i = salto;i < contador; i++){
 
                 if(array[i-salto]>array[i]){ 
-
+                    
                     aux=array[i];
                     array[i]=array[i-salto]; 
                     array[i-salto]=aux; 
@@ -89,5 +97,36 @@ void shell(int array[], int contador){
                 } 
             }
         }
+    }
+}
+
+void swap(int* a, int* b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+int particion (int array[], int bajo, int alto){
+    int pivote, i, j;
+    pivote = array[alto];
+    i = (bajo - 1);
+ 
+    for (j = bajo; j <= alto- 1; j++){
+        if (array[j] <= pivote){
+            i++; 
+            swap(&array[i], &array[j]);
+        }
+    }
+
+    swap(&array[i + 1], &array[alto]);
+    return (i + 1);
+}
+ 
+void quickSort(int array[], int bajo, int alto){
+	int pi;
+    if (bajo < alto){
+        pi = particion(array, bajo, alto);
+        quickSort(array, bajo, pi - 1);
+        quickSort(array, pi + 1, alto);
     }
 }
